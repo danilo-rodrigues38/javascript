b64e5b4d3d2418aca94ff0666e41ab26d85fe94c
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-03-11 10:57:02 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+// var c = 1
+// while (c <= 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+// var c = 1
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// } while (c <= 6)
+
+for (c = 0; c <= 10; c++) {
+    console.log(`Passo ${c}`)
+}
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
